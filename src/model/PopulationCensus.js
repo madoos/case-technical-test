8d4ac538939ed60b4df0d9ab09668beee915cd4d
madoos <maurice.ronet.dominguez@gmail.com>
2016-06-10 20:29:37 +0200

    implementado el schema de poblacion

---
+++ src/model/PopulationCensus.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import loadClass from 'mongoose-class-wrapper';
+ 
+ 
+var censusSchema = new mongoose.Schema({
+      date: {type: Date, required: true},
+      ts: {type: String, required: true},
+      city: {type: String, required: true},
+      population: [
+        {
+          age: {type: Number, required: true},
+          count: {type: Number, required: true} 
+        }
+      ]
+    });
+
+ 
+class censusPopulationModel {
+ 
+ 
+}
+ 
+censusSchema.plugin(loadClass, censusPopulationModel);
+ 
+export default mongoose.model('census-population', censusSchema);
