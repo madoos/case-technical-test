b7ebcc74ca9519df2efd322952d596b6fabe526d
madoos <maurice.ronet.dominguez@gmail.com>
2016-06-12 10:18:52 +0200

    reparado endpoint /population/byrecord/last/

--- src/server/dao/populationByRecord.js
+++ src/server/dao/populationByRecord.js
@@ -3,13 +3,12 @@ import populationModel from '../../model/PopulationCensus';
 const populationDao = {
   getLastRecord: (callback) => {
     const popUnwind = {$unwind:'$population'};
-    const totalPopByCityAndDate = {$group:{ _id: { city : '$city', date: '$date'}, totalPopulation : {$sum: '$population.count'}} };
-    const upLastRecord =  {$sort:{ '_id.date' : -1}};
-    const getLastRecord = {$group:{_id: '$_id.city', recordDate: {$first: '$_id.date'}, totalPopulation: {$first: '$totalPopulation'}} };
-    const setFormat =  {$project:{  city: '$_id', recordDate: '$recordDate', totalPopulation:'$totalPopulation', _id: 0 }};
-    const sortAlphabetically = {$sort:{city: 1}};
+    const totalPopLastRecord = {$group:{_id:{city: '$city', age:'$population.age', date:'$date'},
+                                    totalPopulation:{$sum: '$population.count'}, lastRecord: {$max: '$date'}} };
+    const setFormat = {$project:{ city: '$_id.city', age: '$_id.age', lastRecord: '$lastRecord', totalPopulation: '$totalPopulation', _id: 0} };
+    const sortAscending = {$sort: {city: 1, age:1}};
 
-    const pipeline = [popUnwind, totalPopByCityAndDate, upLastRecord, getLastRecord, setFormat, sortAlphabetically];
+    const pipeline = [popUnwind, totalPopLastRecord, setFormat, sortAscending];
 
     populationModel.aggregate(pipeline, callback);
   }
